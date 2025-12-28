8f9f452ca1b62cda2d1ae5c8d2b6faee1aa544ce
anuskaprajapati <prajapatianuska84@gmail.com>
2025-12-28 12:13:51 +0545

    theme switcher

--- 09themeswitcher/src/components/Card.jsx
+++ 09themeswitcher/src/components/Card.jsx
@@ -4,12 +4,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://i.pinimg.com/736x/24/1c/b6/241cb672af49e117a2cb9060ce88df5f.jpg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Samsung Galaxy S25 Ultra 5G (12GB RAM + 512GB Storage) Titanium Black
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
--- 09themeswitcher/tailwind.config.js
+++ 09themeswitcher/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  ontent: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
